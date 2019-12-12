bd3e8ffb6929303d091b987c70794e90d4f6af87
User <user.email@mail.org>
2019-12-12 12:04:07 +0200

    Project upload

---
+++ TestingExercise/HeroesGame/Implementation/Weapon/BaseWeapon.cs
@@ -0,0 +1,32 @@
+﻿using HeroesGame.Constant;
+
+namespace HeroesGame.Implementation.Weapon
+{
+    public abstract class BaseWeapon
+    {
+        protected BaseWeapon()
+        {
+            this.Damage = WeaponConstants.InitialDamage;
+            this.Level = WeaponConstants.InitialLevel;
+        }
+
+        /// <summary>
+        /// Calculates and returns armor penetration value for each kind of weapon.
+        /// </summary>
+        /// <returns>An integer equal to the amount of armor penetration for given weapon.</returns>
+        public abstract int ArmorPenetration();
+
+        public int Damage { get; set; }
+
+        public int Level { get; set; }
+
+        /// <summary>
+        /// On level up damage gets increased.
+        /// </summary>
+        public void LevelUp()
+        {
+            this.Level++;
+            this.Damage += WeaponConstants.DamagePerLevel;
+        }
+    }
+}
